41a96431c5818a20bd7bb002b81b3cb4897f9849
Ndeta Innocent <you@example.com>
2022-07-23 03:28:30 +0300

    Using hashmaps to update strings

--- main.rs
+++ main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 fn main() {
     println!("Hello, world!");
     //Explicitly declaring the type
@@ -19,8 +20,19 @@ fn main() {
         let scope_v = vec![1, 2, 4, 5, 67, 8, 9];
         //Do stuff with vec
     } //vec goes out of scope and is freed here
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 6);
+    scores.insert(String::from("Red"), 4);
+    for (key, value) in &scores {
+        println!("{}'s score is {}", key, value);
+    }
+
     loop_vec();
     vector_eum();
+    iter_string();
+    overwriting_valuie();
+    update_value_based();
 }
 fn loop_vec() {
     //Immutable
@@ -40,15 +52,52 @@ fn loop_vec() {
 enum SpreadsheeCell {
     Int(i32),
     Float(f32),
-    text(String),
+    Text(String),
 }
 fn vector_eum() {
     let row = vec![
         SpreadsheeCell::Int(4),
         SpreadsheeCell::Float(3.4),
-        SpreadsheeCell::text(String::from("Ndeta")),
+        SpreadsheeCell::Text(String::from("Ndeta")),
     ];
     for sheet in &row {
         println!("Here is the sheet {:?}", sheet);
     }
+}
+fn iter_string() {
+    let t = "mamamaia";
+    for c in t.chars() {
+        println!("{}", c);
+    }
+}
+
+fn vect_tuple_hashmap() {
+    let teams = vec![String::from("Blue"), String::from("Red")];
+    let initial_score = vec![10, 50];
+    // We can use <_,_> because Rust will infer the type as Strini32
+    let mut scores_hashmap: HashMap<_, _> =
+        teams.into_iter().zip(initial_score.into_iter()).collect();
+}
+fn ownershp_in_hashmap() {
+    let field_name = String::from("favorite color");
+    let field_value = String::from("Blue");
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+}
+
+fn overwriting_valuie() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 6);
+    scores.insert(String::from("Blue"), 4);
+    println!("{:?}", scores);
+}
+
+fn update_value_based() {
+    let text = "hello there YouTube";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
 }
